dbede24a6e8bd76717d8e8c09a74096069b170ec
Jairo Nascimento <jaironsousa@gmail.com>
2020-02-11 15:18:11 -0300

    66. Roles de acesso - Parte 1

--- src/main/java/com/jnsdev/wallet/entity/User.java
+++ src/main/java/com/jnsdev/wallet/entity/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable{
 	
 	private String email;
 	
-//	@NotNull
-//	@Enumerated(EnumType.STRING)
-//	private RoleEnum role;
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private RoleEnum role;
 }
